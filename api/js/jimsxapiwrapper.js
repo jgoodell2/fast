1e2d1918ddf4eb0c331203e48d54997c638aa1e7
Jim Goodell <jgoodell2@yahoo.com>
2023-09-17 23:15:29 -0400

    Update jimsxapiwrapper.js

--- api/js/jimsxapiwrapper.js
+++ api/js/jimsxapiwrapper.js
@@ -1,11 +1,11 @@
 
-         function setupSimpleXAPI(actorName, actorMail, verbId, verbDisplay, objectId, objectName, objectDescription, resultResponseValue, resultSuccessValue, contextActivity, contextActivityName){
+         function setupSimpleXAPI(actorName, actorOpenId, verbId, verbDisplay, objectId, objectName, objectDescription, resultResponseValue, resultSuccessValue, contextActivity, contextActivityName){
 
                 let userAgent = JSON.stringify(navigator.userAgent);
 
                  var statement = {
                      "actor": {
-                         "mbox": actorMail,
+                         "openId": actorOpenId,
                          "name": actorName,
                          "objectType": "Agent"
                      },
